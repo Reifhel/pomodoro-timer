fe07d78bebb66f5d180f2c32d1a513d1103b9f3a
Reifhel <rafaelherdt1@gmail.com>
2025-08-09 15:55:24 -0300

    fix: fixed the range on form to 5 - 60 minutes cycles

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -28,7 +28,7 @@ export function NewCycleForm() {
         id="minutos amount"
         placeholder="00"
         step={5}
-        min={1}
+        min={5}
         max={60}
         disabled={!!activeCycle}
         {...register("minutesAmount", { valueAsNumber: true })}
